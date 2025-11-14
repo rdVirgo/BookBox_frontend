138650de345361a4fc65f87d284423b31dc1e12d
22210383t <radia.merabtene@etu.univ-tours.fr>
2025-11-14 15:52:43 +0100

    creating the differents components and interfaces

---
+++ bookbox_frontend/src/Component/coordinates/coordinates.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coordinates',
+  standalone: true,
+  imports: [],
+  templateUrl: './coordinates.component.html',
+  styleUrl: './coordinates.component.css'
+})
+export class CoordinatesComponent {
+
+}
