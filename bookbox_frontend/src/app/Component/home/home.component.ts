b0a0dcaa8b203d66d5bb333e55c56cd796593998
Mamadou Coulibaly <djiguicmd1960cmd@gmail.com>
2025-11-28 17:39:22 +0100

    Home page build

--- bookbox_frontend/src/app/Component/home/home.component.ts
+++ bookbox_frontend/src/app/Component/home/home.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
