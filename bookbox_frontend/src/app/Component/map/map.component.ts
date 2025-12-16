266c3bd61a7170829ac23bc3e034df2ea8070f21
radia <radia.merabtene@etu.univ-tours.fr>
2025-12-16 15:04:36 +0100

    coordinates uploaded to map

--- bookbox_frontend/src/app/Component/map/map.component.ts
+++ bookbox_frontend/src/app/Component/map/map.component.ts
@@ -1,15 +1,17 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
-
+import { HttpClientModule } from '@angular/common/http';
+import { CoordinatesService } from '../../service/CoordinatesService/coordinates.service';
+import {icon} from "leaflet";
 @Component({
   selector: 'app-map',
+  imports: [HttpClientModule],
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css'],
   standalone: true,
 })
 export class MapComponent implements AfterViewInit {
   private map!:any;
-
   private initMap(): void {
     this.map = L.map('map', {
       center: [46.603354, 1.888334],
@@ -25,9 +27,36 @@ export class MapComponent implements AfterViewInit {
     tiles.addTo(this.map);
   }
 
-  constructor() { }
+  constructor(private coordinatesService: CoordinatesService) { }
 
+  private loadCoordinates():void {
+    const customIcon = L.icon({
+      iconUrl: 'https://geekcommunicant.com/assets/files/images/marker.png',
+      iconSize: [32, 48],
+      iconAnchor: [16, 42],
+      shadowUrl: 'https://geekcommunicant.com/assets/files/images/marker-shadow.png',
+      shadowSize: [50, 48],
+      shadowAnchor: [12, 44]
+    });
+
+    this.coordinatesService.getAllCoordinates().subscribe(
+      {
+        next: (coordinates) => {
+          coordinates.forEach(coord => {
+            // casting to numbers because in the backend they are strings
+            L.marker([+coord.latitude, +coord.longitude],{icon : customIcon
+            }).addTo(this.map).bindPopup('boite');
+          });
+        },
+        error : (err) => {
+          console.error("failure while uploading the coordinate ", err);
+        }
+      }
+    );
+  }
   ngAfterViewInit(): void {
     this.initMap();
+    this.loadCoordinates();
   }
+
 }
